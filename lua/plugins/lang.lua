af8b25347ea4e2be353b3ab0ef26080edbf6d6d2
Tran Duc Binh <binhtran432k@gmail.com>
2023-05-22 19:51:49 +0700

    lang: add csharp support

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -40,6 +40,42 @@ return {
     },
   },
 
+  -- csharp
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "Hoffs/omnisharp-extended-lsp.nvim" },
+    opts = {
+      servers = {
+        omnisharp = {
+          cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+          capabilities = { textDocument = { formatting = true } },
+          on_attach = function(client, bufnr)
+            -- stylua: ignore
+            vim.keymap.set("n", "gd", "<cmd>lua require('omnisharp_extended').telescope_lsp_definitions()<cr>", { desc = "Goto definition (omnisharp)", buffer = bufnr })
+
+            local function toSnakeCase(str)
+              return string.gsub(str, "%s*[- ]%s*", "_")
+            end
+
+            local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+            for i, v in ipairs(tokenModifiers) do
+              tokenModifiers[i] = toSnakeCase(v)
+            end
+            local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+            for i, v in ipairs(tokenTypes) do
+              tokenTypes[i] = toSnakeCase(v)
+            end
+          end,
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+
   -- python
   {
     "neovim/nvim-lspconfig",
