e62aa3fa0bb45bca0acdbae2faa71af4b2d500cb
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-09 16:08:44 +0700

    feat(zig): add zig support

---
+++ lua/plugins/lang/zig.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@type PluginLspOpts
+    opts = {
+      servers = {
+        zls = {},
+      },
+    },
+  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     if type(opts.ensure_installed) == "table" then
+  --       vim.list_extend(opts.ensure_installed, { "zig" })
+  --     end
+  --   end,
+  -- },
+}
