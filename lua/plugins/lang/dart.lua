f7c7f4403248fe64394589df25ba3fcbc0128fb3
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-30 20:51:32 +0700

    feat(dart): add language support

---
+++ lua/plugins/lang/dart.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dartls = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "dart" })
+      end
+    end,
+  },
+}
