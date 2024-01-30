7fd78e755db8464ffceb18db261c82f7c3d4b421
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-30 20:52:12 +0700

    refactor(markdown): use custom config

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,3 +1,31 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.markdown" },
+  -- { import = "lazyvim.plugins.extras.lang.markdown" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
+      end
+    end,
+  },
+
+  -- Markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
