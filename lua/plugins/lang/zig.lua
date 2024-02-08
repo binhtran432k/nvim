3b3038d1a2c5cc631fcbf12178c121297c387075
Binh Duc Tran <binhtran432k@gmail.com>
2024-02-08 12:04:14 +0700

    refactor(lang/zig): uncomment treesitter

--- lua/plugins/lang/zig.lua
+++ lua/plugins/lang/zig.lua
@@ -8,12 +8,12 @@ return {
       },
     },
   },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = function(_, opts)
-  --     if type(opts.ensure_installed) == "table" then
-  --       vim.list_extend(opts.ensure_installed, { "zig" })
-  --     end
-  --   end,
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "zig" })
+      end
+    end,
+  },
 }
