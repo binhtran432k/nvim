05339d1acce2186295361b41400f577a2608afcf
Binh Duc Tran <binhtran432k@gmail.com>
2024-10-10 18:47:02 +0700

    feat(ungrammar): use lspconfig config

--- lua/plugins/lang/ungrammar.lua
+++ lua/plugins/lang/ungrammar.lua
@@ -9,23 +9,6 @@ return {
       })
     end,
     opts = function(_, opts)
-      local lspconfig = require("lspconfig")
-      require("lspconfig.configs").ungrammar_languageserver = {
-        default_config = {
-          -- replace it with true path
-          cmd = { "ungrammar-languageserver", "--stdio" },
-          filetypes = { "ungrammar" },
-          single_file_support = true,
-          root_dir = lspconfig.util.root_pattern(".git"),
-          settings = {
-            ungrammar = {
-              validate = {
-                enable = true,
-              },
-            },
-          },
-        },
-      }
       opts.servers = vim.tbl_deep_extend("keep", opts.servers or {}, {
         ungrammar_languageserver = {},
       })
